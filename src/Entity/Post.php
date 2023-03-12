879e82c459e650d5bb03c2d7abd94582630eeca0
alicekaeva <alikaeva.aa@students.dvfu.ru>
2023-03-12 23:29:35 +1000

    added some entities

---
+++ src/Entity/Post.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+class Post
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 500)]
+    private ?string $content = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $postingDate = null;
+
+    #[ORM\ManyToOne(inversedBy: 'posts')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $author = null;
+
+    #[ORM\ManyToMany(targetEntity: User::class, inversedBy: 'favorites')]
+    private Collection $addedToFav;
+
+    #[ORM\ManyToMany(targetEntity: Tag::class, inversedBy: 'relatedPosts')]
+    private Collection $tags;
+
+    public function __construct()
+    {
+        $this->addedToFav = new ArrayCollection();
+        $this->tags = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getPostingDate(): ?\DateTimeInterface
+    {
+        return $this->postingDate;
+    }
+
+    public function setPostingDate(\DateTimeInterface $postingDate): self
+    {
+        $this->postingDate = $postingDate;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?User $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getAddedToFav(): Collection
+    {
+        return $this->addedToFav;
+    }
+
+    public function addAddedToFav(User $addedToFav): self
+    {
+        if (!$this->addedToFav->contains($addedToFav)) {
+            $this->addedToFav->add($addedToFav);
+        }
+
+        return $this;
+    }
+
+    public function removeAddedToFav(User $addedToFav): self
+    {
+        $this->addedToFav->removeElement($addedToFav);
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Tag>
+     */
+    public function getTags(): Collection
+    {
+        return $this->tags;
+    }
+
+    public function addTag(Tag $tag): self
+    {
+        if (!$this->tags->contains($tag)) {
+            $this->tags->add($tag);
+        }
+
+        return $this;
+    }
+
+    public function removeTag(Tag $tag): self
+    {
+        $this->tags->removeElement($tag);
+
+        return $this;
+    }
+}
