bb0b9e59885b2cc88e39d9861ebbd0d0e23cabf6
alicekaeva <alikaeva.aa@students.dvfu.ru>
2023-04-09 20:52:00 +1000

    smol russification and attempt to hide login and register path from logged user

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -40,11 +40,11 @@ class RegistrationFormType extends AbstractType
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Пожалуйста, введите пароль',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Ваш пароль должен быть как минимум {{ limit }} символов',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
