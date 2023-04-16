8ff62c8f2ea39a01e51056a623ec4dbb4a514ba8
alicekaeva <alikaeva.aa@students.dvfu.ru>
2023-04-16 21:34:07 +1000

    prettify

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'Пользователь с такой почтой уже существует')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -357,5 +357,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $this->interests = $interests;
 
         return $this;
+    }
+
+    public function __toString()
+    {
+        return $this->getFullName() . PHP_EOL;
     }
 }
