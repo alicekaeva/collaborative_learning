9b74646d3ce132d31c8b6b86a73e79d4fb8c51e4
alicekaeva <alikaeva.aa@students.dvfu.ru>
2023-05-04 11:27:49 +1000

    started group management

--- src/Controller/GroupController.php
+++ src/Controller/GroupController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Admin;
 use App\Entity\Group;
+use App\Entity\Message;
 use App\Form\GroupType;
+use App\Repository\AdminRepository;
 use App\Repository\GroupRepository;
+use App\Repository\MessageRepository;
+use App\Repository\UserRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -24,13 +29,25 @@ class GroupController extends AbstractController
 
     #[Route('/new', name: 'app_group_new', methods: ['GET', 'POST'])]
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
-    public function new(Request $request, GroupRepository $groupRepository): Response
+    public function new(Request $request, GroupRepository $groupRepository, AdminRepository $adminRepository, UserRepository $userRepository): Response
     {
         $group = new Group();
         $form = $this->createForm(GroupType::class, $group);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $user = $this->getUser();
+            $admin = $adminRepository->findOneBy(['user' => $user]);
+            if (!$admin) {
+                $admin = new Admin();
+                $admin->setUser($user);
+                $adminRepository->save($admin, true);
+                $userRoles = $user->getRoles();
+                $userRoles[] = 'ROLE_ADMIN';
+                $user->setRoles($userRoles);
+                $userRepository->save($user, true);
+            }
+            $group->setAdministrator($admin);
             $groupRepository->save($group, true);
 
             return $this->redirectToRoute('app_group_index', [], Response::HTTP_SEE_OTHER);
@@ -40,6 +57,25 @@ class GroupController extends AbstractController
             'group' => $group,
             'form' => $form,
         ]);
+    }
+
+    #[Route('/enroll', name: 'app_group_enroll', methods: ['POST'])]
+    public function enroll(Request $request, MessageRepository $messageRepository, UserRepository $userRepository): Response
+    {
+        $user = $this->getUser();
+        $receiverId = $request->request->get('receiver_id');
+        $receiver = $userRepository->findOneBy(['id'=>$receiverId]);
+        $groupName = $request->request->get('group_name');
+        $content = 'Привет! Я хочу присоединиться к вашей группе ' . $groupName . '. Можешь добавить меня?';
+
+        $message = new Message();
+        $message->setSender($user);
+        $message->setReceiver($receiver);
+        $message->setContent($content);
+        $message->setSendingDate(new \DateTimeImmutable());
+        $messageRepository->save($message, true);
+
+        return $this->redirectToRoute('app_message_show', ['id' => $receiverId]);
     }
 
     #[Route('/{id}', name: 'app_group_show', methods: ['GET'])]
@@ -73,7 +109,7 @@ class GroupController extends AbstractController
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function delete(Request $request, Group $group, GroupRepository $groupRepository): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$group->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $group->getId(), $request->request->get('_token'))) {
             $groupRepository->remove($group, true);
         }
 
